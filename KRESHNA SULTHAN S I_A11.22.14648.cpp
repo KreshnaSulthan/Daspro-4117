b062ac4878d1811000dc7e2641a37affd8670f87
KreshnaSulthan <112914832+KreshnaSulthan@users.noreply.github.com>
2022-09-14 16:31:10 +0700

    Add files via upload
    KRESHNA SULTHAN S I_A11.22.14648

---
+++ KRESHNA SULTHAN S I_A11.22.14648.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "kreshna sulthan syah islamey\nA11.2022.14648\nSMA N 1 UMGARAN" << endl;
+    int u= 19;
+    float t= 171;
+    char k= 'L';
+    string l= "laki laki";
+    cout << "\numur :" << u;
+    cout << "\ntinggi :" << t;
+    cout << "\nkelamin :" << k;
+    cout << "\nkelamin :" << l;
+
+    return 0;
+
+}
