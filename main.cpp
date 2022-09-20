e5e92f10fc972ba5e1274e9c66e0b4df85648f11
KreshnaSulthan <112914832+KreshnaSulthan@users.noreply.github.com>
2022-09-20 18:04:00 +0700

    Add files via upload
    TUGAS PERTEMUAN 5
    Kreshna Sulthan Syah Islamey
    (A11.2022.14648)

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout <<"==========================================================================================="<< endl;
+    cout << "   \t\t\t PERTEMUAN 5 MEMBUAT PROGRAM LUAS LINGKARAN \t\t\t " << endl;
+    cout <<" \t\t\t\t\t\t\t\t\t\t\t " <<endl;
+    cout << "     NAMA : KERSHNA SULTHAN S I , A11202214648 \t\t\t\t\t\t  " << endl;
+    cout <<"   \t\t\t\t\t\t\t\t\t\t\t " <<endl;
+
+    float n,r,hasil;
+    n = 3.14;
+    r = 16;
+    cout <<"\t a. Masukan phi       \t: ";
+    cin >> n;
+    cout <<" \t b. Masukan rusuk  \t: ";
+    cin >> r;
+    cout <<" \t--> Hasilnya adalah \t: " << n*r << endl;
+    cout <<"==========================================================================================="<<endl;
+    return 0;
+}
